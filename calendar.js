27eeeb8fbb2d62326b8742b39bbb2a9e137beb36
Talha <muhammad.t.k@hotmail.co.uk>
2020-10-12 22:14:54 -0400

    prayer calendar
    added prayer timings for each date in calendar.

--- calendar.js
+++ calendar.js
@@ -12,19 +12,47 @@ async function getPrayerTimesCalendar(long, lat) {
     console.log(api.data.length);
     // calendar date
     let dates = [];
+    let fajrTime = [];
+    let dhuhrTime = [];
+    let asrTime = [];
+    let maghribTime = [];
+    let ishaTime = [];
+    let sunriseTime = [];
     for (let i = 0; i < api.data.length; i++) {
-        // console.log(obj);
-        // console.log(api.data[i].date.gregorian.day);
+        // add dates of current month to array
         dates.push(api.data[i].date.gregorian.day);
+
+        // add timings of each prayer to
+        fajrTime.push(api.data[i].timings.Fajr);
+        dhuhrTime.push(api.data[i].timings.Dhuhr);
+        asrTime.push(api.data[i].timings.Asr);
+        maghribTime.push(api.data[i].timings.Maghrib);
+        ishaTime.push(api.data[i].timings.Isha);
+
+        // sunrise
+        sunriseTime.push(api.data[i].timings.Sunrise);
     }
-    console.log(dates);
+    // console.log(fajrTime);
     let table = document.getElementById('calendar-body');
     for (let i = 0, row; row = table.rows[i]; i++) {
         let calDate = row.getElementsByTagName('th');
-        // console.log(calDate[0]);
+        let dateFajr = row.getElementsByClassName('fajr-prayer');
+        let dateDhuhr = row.getElementsByClassName('dhuhr-prayer');
+        let dateAsr = row.getElementsByClassName('asr-prayer');
+        let dateMaghrib = row.getElementsByClassName('maghrib-prayer');
+        let dateIsha = row.getElementsByClassName('isha-prayer');
+        let dateSunrise = row.getElementsByClassName('sunrise');
+
         calDate[0].innerHTML = dates[i];
+        dateFajr[0].innerHTML = fajrTime[i];
+        dateDhuhr[0].innerHTML = dhuhrTime[i];
+        dateAsr[0].innerHTML = asrTime[i];
+        dateMaghrib[0].innerHTML = maghribTime[i];
+        dateIsha[0].innerHTML = ishaTime[i];
+        dateSunrise[0].innerHTML = sunriseTime[i];
+
         for (let j = 0, col; col = row.cells[j]; j++) {
-            // console.log(col);
+
         }
     }
     // prayer timings
