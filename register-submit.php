4b76b5495ecbbbf4f6a7435b376c64053aed2de7
Talha <muhammad.t.k@hotmail.co.uk>
2021-01-16 20:57:28 -0500

    user registration
    +footer
    +styling for user registration page

---
+++ register-submit.php
@@ -0,0 +1,80 @@
+<?php
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'mysalatlogin';
+
+$success = "";
+$submitted = false;
+$email_err = $password_err = "";
+
+// connect to db using above info
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+    // stop the script if there is an error connecting
+    exit('Failed to connect: ' . mysqli_connect_error());
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// check if the data was submitted, isset() checks if the data exists
+    if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+        // user did not enter required data
+        exit();
+//    exit('Please complete the registration form!');
+    }
+// check if registration values are not empty
+    if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+        // one or more of the values are empty
+        exit();
+//    exit('Please complete the registration form!');
+    }
+
+    /***************form validation***************/
+// email validation
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        exit('Email is not valid!');
+    }
+// check for invalid characters
+    if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
+        exit('Username not valid!');
+    }
+//check character length
+    if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+        $password_err = 'Password must be between 5 and 20 characters long!';
+//    exit('Password must be between 5 and 20 characters long!');
+//        exit();
+    }
+
+// check if account alreadt exists
+    if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        $stmt->store_result();
+        // storing the result to see if the account exists in the database
+        if ($stmt->num_rows > 0) {
+            // username already exists
+            echo 'Username already exists!';
+        } else {
+            // insert a new account
+            if (empty($password_err)) {
+                if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
+                    // to avoid exposing any passwords in our database, we hash them and use password_hash when a user logs in
+                    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                    $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+                    $stmt->execute();
+                    $success = 'Registration Successful! You may now login.';
+                    $submitted = true;
+//                echo 'Registration successful!';
+                } else {
+                    echo 'Could not prepare statement!';
+                }
+            }
+            $stmt->close();
+        }
+    } else {
+        // problem in sql statement, check and make sure the accounts table exists
+        echo 'Could not prepare the statement!';
+    }
+    $con->close();
+}
+?>
---
+++ register.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Adhan App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"-->
+<!--          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+          integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <link rel="stylesheet" href="./register.css">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./footer.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.js"
+            integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+    <div>
+        <h1 id="main-title">My Salat</h1>
+        <div class="timer">
+            <h2 id="next-prayer-text"></h2>
+            <h2 id="next-prayer"></h2>
+        </div>
+    </div>
+    <div class="reg">
+        <a class="btn"><span>Login</span></a>
+        <a href="register.php" class="btn"><span>Register</span></a>
+    </div>
+</header>
+<div class="main">
+    <div class="register-container">
+        <div class="register-title">Register for My Salat</div>
+        <form method="post" autocomplete="off" id="reg-form">
+            <label for="username">
+                <i class="fas fa-user"></i>
+            </label>
+            <input type="text" name="username" placeholder="Username" id="username" required>
+            <label for="password">
+                <i class="fas fa-lock"></i>
+            </label>
+            <input type="password" name="password" placeholder="Password" id="password" required>
+            <label for="email">
+                <i class="fas fa-envelope"></i>
+            </label>
+            <input type="email" name="email" placeholder="Email" id="email" required>
+            <input type="submit" value="Register" id="myBtn">
+        </form>
+        <script>
+            $(function () {
+
+                $('form').on('submit', function (e) {
+
+                    e.preventDefault();
+
+                    $.ajax({
+                        type: 'post',
+                        url: 'register-submit.php',
+                        data: $('form').serialize(),
+                        success: function () {
+                            $("#reg-form").html("<div id='message'></div>");
+                            $("#message")
+                                .html("<h2>Registration Successful! You may now login.</h2>");
+                        }
+                    });
+
+                });
+
+            });
+        </script>
+    </div>
+</div>
+<div class="footer-container">
+    <footer>
+        <div class="footnote">
+            My Salat <span class="copyright">© 2021</span> All rights reserved.
+            <p>Read our <a href="index.html">Terms and Conditions</a> and <a href="index.html">Privacy Policies</a></p>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
